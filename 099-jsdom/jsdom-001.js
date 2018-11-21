84780bde0126f7e34fd65588d584ac35155ec2ab
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-11-21 15:10:36 +0800

    updates

--- 099-jsdom/jsdom-001.js
+++ 099-jsdom/jsdom-001.js
@@ -1,7 +1,12 @@
-const log = console.log;
 const _jsdom = require('jsdom');
 const { JSDOM } = _jsdom;
+
+//log([_jsdom,JSDOM]);
 
 const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
 console.log(dom.window.document.querySelector("p").textContent); 
 // Hello World
+
+//log(dom.window);
+//log(dom.window.document);
+//log(dom.window.document.__proto__);
