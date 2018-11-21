f864f8fa112756d5c824495aa2d7f865d0d0d63f
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-11-21 15:29:35 +0800

    run color

--- run.js
+++ run.js
@@ -1,4 +1,27 @@
+var theOutput = (function iifeEnvDetect(){
+  var output = false;
+  if ( typeof global !== 'undefined' && typeof module !== 'undefined' && typeof require !== 'undefined' ) {
+    if ( require.main === module ) {
+      output = 'node';      
+    } else {
+      output = 'node-module';
+    }
+  }
+
+  if ( typeof window !== 'undefined' && typeof document !== 'undefined' ) {
+    output = 'browser';
+    if ( typeof require === 'undefined' ) {
+      //require = function( value ) { console.log("%c TO REQUIRE : " + value,"color: ##FF0000;"); }
+      require = function( value ) { console.log("TO REQUIRE : " + value); }
+    }
+  }
+  return output;
+})();
+
 var log = console.log;
+var fs = require('fs');
+
+log(theOutput);
 
 var uu = {};
 uu.refP = (function exec(){
@@ -12,8 +35,6 @@ uu.refP = (function exec(){
   return reflectProperties;
 })();
 uu.reflectProperties = uu.refP;
-
-var fs = require('fs');
 
 //log(uu.refP( console ));
 
