3f10da368cba1c224d491b47908465465707369d
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-12-05 12:23:40 +0800

    Additional  Checks , Function

--- 000-core/check-core.js
+++ 000-core/check-core.js
@@ -4,20 +4,6 @@
 var assert = require('assert');
 var log = console.log;
 
-//log(Object.prototype);   //{}
-//log(Object.constructor); //[Function: Function]
-//log(Object.__proto__);   //[Function]
-//log(Object.constructor.prototype === Object.__proto__); // Object.constructor.prototype === Object.__proto__
-
-//log({}.prototype);   // undefined
-//log({}.constructor); // [Function: Object]
-//log({}.__proto__);   // {}
-//log({}.constructor.prototype); // {}
-
-//log(function(){}.prototype);   // {}
-//log(function(){}.constructor); // [Function: Function]
-//log(function(){}.__proto__);   // [Function]
-
 describe("js core properties",function(){
 
 	it("js environment",function(){
@@ -124,3 +110,42 @@ describe("js core properties",function(){
 	});
 
 });
+
+
+//log(Object.prototype);   //{}
+//log(Object.constructor); //[Function: Function]
+//log(Object.__proto__);   //[Function]
+//log(Object.constructor.prototype === Object.__proto__); // Object.constructor.prototype === Object.__proto__
+
+//log({}.prototype);   // undefined
+//log({}.constructor); // [Function: Object]
+//log({}.__proto__);   // {}
+//log({}.constructor.prototype); // {}
+
+//log(function(){}.prototype);   // {}
+//log(function(){}.constructor); // [Function: Function]
+//log(function(){}.__proto__);   // [Function]
+
+//log([].prototype);   // undefined
+//log([].constructor); // [Function: Array]
+//log([].__proto__);   // []
+
+function theFunction(a,b,c,d,e)  {
+	var test = true;
+}
+
+function thatFunction(
+	a,
+	b,
+	c,
+	d,
+	e,
+	f,
+) {
+	var helloWorld = 'hello world';
+}
+
+log(theFunction.toString().split("{"));
+log(theFunction.a);
+
+log(thatFunction.toString().split("{"));
