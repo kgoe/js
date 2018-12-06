202e6097ade9dea948be6dbdcffab150de57e0ab
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-12-06 20:06:19 +0800

    jsdom

--- 011-js-jsdom/jsdom-000.js
+++ 011-js-jsdom/jsdom-000.js
@@ -1,5 +1,11 @@
+const log = console.log;
 const _jsdom = require('jsdom');
 const { JSDOM } = _jsdom;
+
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+
+console.log(dom.window.document.querySelector("p").textContent); 
+// Hello World
 
 // console.log(_jsdom);
 // dom jsdom
--- jsdom-000.js
+++
@@ -1,7 +0,0 @@
-const log = console.log;
-const _jsdom = require('jsdom');
-const { JSDOM } = _jsdom;
-
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent); 
-// Hello World
