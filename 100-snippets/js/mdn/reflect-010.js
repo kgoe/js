435b75d8a19b9dbbbbec087b5b137ab12a56abd0
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-12-09 00:10:45 +0800

    proxy , reflect

---
+++ 100-snippets/js/mdn/reflect-010.js
@@ -0,0 +1,13 @@
+// Object
+var obj = { x: 1, y: 2 };
+Reflect.get(obj, 'x'); // 1
+
+// Array
+Reflect.get(['zero', 'one'], 1); // "one"
+
+// Proxy with a get handler
+var x = {p: 1};
+var obj = new Proxy(x, {
+  get(t, k, r) { return k + 'bar'; }
+});
+Reflect.get(obj, 'foo'); // "foobar"
