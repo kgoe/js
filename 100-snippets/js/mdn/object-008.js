1873d6f1f9c8f3da45366b86dd660063cdec1bff
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-12-10 18:01:46 +0800

    Object.defineProperty

--- 100-snippets/js/mdn/object-008.js
+++ 100-snippets/js/mdn/object-008.js
@@ -4,6 +4,7 @@ var v2 = true;
 var v3 = 10;
 var v4 = Symbol('foo');
 
+// https://stackoverflow.com/questions/42091600/how-to-merge-object-in-ie-11
 var obj = Object.assign({}, v1, null, v2, undefined, v3, v4);
 // Primitives will be wrapped, null and undefined will be ignored.
 // Note, only string wrappers can have own enumerable properties.
---
+++ 100-snippets/js/mdn/object-039.js
@@ -0,0 +1,16 @@
+function myclass() {
+}
+
+Object.defineProperty(myclass.prototype, "x", {
+  get() {
+    return this.stored_x;
+  },
+  set(x) {
+    this.stored_x = x;
+  }
+});
+
+var a = new myclass();
+var b = new myclass();
+a.x = 1;
+console.log(b.x); // undefined
