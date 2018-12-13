6287dbcc7f817d5a8b60aca39ec10633fa05c74f
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2018-12-13 18:20:37 +0800

    executor

---
+++ exec-script.js
@@ -0,0 +1,46 @@
+;
+/**
+ * exec-script.js
+ * this script aims to execute scripts contained in this project folder
+ * using node.js executable
+ */
+
+const {exec} = require('child_process');
+
+
+/**
+ * executor
+ * @param {string} command
+ */
+function executor( command ) {
+
+  /**
+   * execCallback
+   * @param {*} err
+   * @param {*} stdout
+   * @param {*} stderr
+   * @return {*}
+   */
+  function execCallback(err, stdout, stderr) {
+    if (err) {
+      // node couldn't execute the command
+      console.log(`error: ${err}`);
+      return;
+    } else {
+      // the *entire* stdout and stderr (buffered)
+      console.log(`===stdout===`);
+      console.log(`${stdout}`);
+      console.log(`===stderr===`);
+      console.log(`${stderr}`);
+      return;
+    }
+  }
+
+  exec(command, execCallback);
+}
+
+var command = 'php -v';
+executor(command);
+
+var command = 'node -v';
+executor(command);
