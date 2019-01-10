1fcd9b8934e6c8a0fdc2ed67645f04a23bb3cb85
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-10 20:01:49 +0800

    libraries and trends to do

---
+++ 004-js-library/doc-libraries.js
@@ -0,0 +1,69 @@
+;
+
+var doc = {};
+
+doc['lifehack'] =
+`
+date
+category
+flag
+unique identifier
+special code
+
+last modified date
+created date
+last presented date
+
+dash and underscores
+
+_DRAFT
+_VER1
+_VER2
+_SDF
+`;
+
+doc['2019-trends'] =
+`
+Modular design and lower development costs
+Responsive design and the rise of mobile users
+Chatbots and customer support
+Accelerated mobile pages and load speed
+Voice search optimization
+API-first design and streamlined workloads
+Motion UI and suffering attention spans
+Artificial intelligence and cybersecurity
+
+https://www.coredna.com/blogs/web-development-trends
+https://medium.com/datadriveninvestor/web-development-trends-in-2019-85b77610e573
+https://www.hexacta.com/2018/12/10/software-development-trends-2019/
+http://customerthink.com/trends-you-can-expect-in-web-development-for-2019/
+https://www.techaheadcorp.com/blog/mobile-app-development-trends-2019/
+https://dashbouquet.com/blog/web-development/web-development-trends-that-will-dominate-in-2019
+https://leaderchat.org/2018/11/27/2019-learning-development-trends-10-expert-predictions/
+https://auxesisinfotech.com/Web-Development-Trends-2019
+https://www.youtube.com/watch?v=VLm3Y7Odb74
+https://merehead.com/blog/web-development-trends-2019/
+https://www.techjini.com/blog/mobile-app-development-trends-and-predictions-2019/
+https://polcode.com/blog/software-development-trends-for-2019/
+https://www.peerbits.com/blog/top-mobile-app-development-trends.html
+https://www.netsolutions.com/insights/12-mobile-app-development-trends-to-watch-out-for-in-2019/
+https://www.netsolutions.com/insights/12-mobile-app-development-trends-to-watch-out-for-in-2019/
+https://www.quora.com/What-will-be-the-latest-web-app-development-trends-in-2019
+https://electricsilk.com/website-design-and-development-trends-for-2019/
+https://www.forbes.com/sites/forbeshumanresourcescouncil/2018/09/24/seven-learning-and-development-trends-to-adopt-in-2019/#423230ee104b
+`;
+
+doc['links'] =
+`
+https://hackernoon.com/which-top-javascript-trends-to-look-in-2019-528072bf0082
+https://scotch.io/bar-talk/10-node-frameworks-to-use-in-2019
+https://blog.bitsrc.io/the-most-in-demand-javascript-frameworks-for-developers-in-2019-39bba192d8dc
+https://blog.bitsrc.io/comparing-http-request-libraries-for-2019-7bedb1089c83
+https://www.valuecoders.com/blog/technology-and-apps/top-15-front-end-development-tools-2019/
+https://www.sitepoint.com/top-javascript-frameworks-libraries-tools-use/
+https://technostacks.com/blog/javascript-frameworks/
+https://www.quora.com/What-are-the-top-JavaScript-libraries-tech-to-learn-in-2019
+https://www.quora.com/Is-Vue-js-going-to-take-over-React-in-2019
+https://medium.com/zerotomastery/top-libraries-tech-to-learn-in-2019-for-full-stack-developers-f8c0331b8a00
+https://www.coredna.com/blogs/web-development-trends
+`;
