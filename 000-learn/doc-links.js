46e319ce18de41282c72d47f6bcd285ac61c3855
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-14 12:50:08 +0800

    youtube

--- 000-learn/doc-links.js
+++ 000-learn/doc-links.js
@@ -13,6 +13,11 @@ orbs from circcleci
 
 doc['link'] =
 `
+`;
+
+doc['link-youtube'] =
+`
 https://www.youtube.com/watch?v=liJbB_0eCTo
 https://www.youtube.com/watch?v=DSq9uGs_z0E
+https://www.youtube.com/watch?v=rrkrvAUbU9Y
 `;
