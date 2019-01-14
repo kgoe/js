7ac90fb913b622ce388d253f8264a25f7ebb03e8
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-14 19:55:10 +0800

    jsdoc

---
+++ 000-learn/jsdoc.js
@@ -0,0 +1,78 @@
+;
+var doc = {};
+doc['tags'] =
+`
+@abstract @virtual
+@access
+@alias
+@async
+@augments @extends
+@author
+@borrows
+@callback
+@class @constructor
+@classdesc
+@constant @const
+@constructs
+@copyright
+@default @defaultvalue
+@deprecated
+@description @desc
+@enum
+@event
+@example
+@exports
+@external @host
+@file @fileoverview @overview
+@fires @emits
+@function @func @method
+@generator
+@global
+@hideconstructor
+@ignore
+@implements
+@inheritdoc
+@inner
+@instance
+@interface
+@kind
+@lends
+@license
+@listens
+@member @vaar
+@memberof
+@mixes
+@mixin
+@module
+@name
+@namespace
+@override
+@package
+@param @arg @argument
+@private
+@property @prop
+@protected
+@public
+@readonly
+@requires
+@returns @return
+@see
+@since
+@static
+@summary
+@this
+@throws @exception
+@todo
+@tutorial
+@type
+@typedef
+@variation
+@version
+@yields @yield
+`;
+
+doc['tags-inline'] =
+`
+@link
+@tutorial
+`;
