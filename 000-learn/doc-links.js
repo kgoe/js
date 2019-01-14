b6c2b602c9a5afb049444f5899197e0a9a86d674
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-14 19:28:34 +0800

    links updates

--- 000-learn/doc-links.js
+++ 000-learn/doc-links.js
@@ -17,8 +17,4 @@ doc['link'] =
 
 doc['link-youtube'] =
 `
-https://www.youtube.com/watch?v=liJbB_0eCTo
-https://www.youtube.com/watch?v=DSq9uGs_z0E
-https://www.youtube.com/watch?v=rrkrvAUbU9Y
-https://www.youtube.com/watch?v=20adDr7Felw
 `;
