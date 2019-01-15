02d63f368b9443c81bcc3ee019d595fb0ae1be12
kgoe-mobile <kgoespiritu@gmail.com>
2019-01-15 20:15:47 +0800

    links update

--- 000-learn/doc-links.js
+++ 000-learn/doc-links.js
@@ -13,6 +13,9 @@ orbs from circcleci
 
 doc['link'] =
 `
+https://scotch.io/bar-talk/10-node-frameworks-to-use-in-2019
+https://medium.com/cuelogic-technologies/top-3-best-javascript-frameworks-for-2019-3e6d21eff3d0
+https://www.businesswire.com/news/home/20181206005188/en/npm-Announces-2019-Predictions-JavaScript
 `;
 
 doc['link-youtube'] =
---
+++ prj-files.js
@@ -0,0 +1,30 @@
+;
+
+/*
+todo
+list all standard files in the project
+
+# entry points
+index.js
+test.js
+
+# config files
+example.env
+.eslintrc
+.env
+.gitignore
+.dockerignore
+packages.json
+dockerfile
+docker-compose.yml
+bitbucket-pipelines.yml
+
+# library files
+composer.phar
+phpunit.phar
+
+# readme files
+README.md
+LICENSE
+
+*/
