be221b3874abcfc492f41cc2d38f5b40490b46b0
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-25 19:17:32 +0800

    aws sdk update

---
+++ 004-js-library/sdk/aws-sdk.js
@@ -0,0 +1,5 @@
+// https://aws.amazon.com/sdk-for-node-js/
+// https://aws.amazon.com/developers/getting-started/nodejs/
+// https://docs.aws.amazon.com/sdk-for-javascript/index.html#lang/en_us
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/index.html
+// https://forums.aws.amazon.com/forum.jspa?forumID=148
