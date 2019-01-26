1ff4478fe99192647986f19479ffc1ce9b242813
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-26 22:56:43 +0800

    google modules

---
+++ 004-js-library/doc/doc-google-cdn-modules.js
@@ -0,0 +1,140 @@
+;
+
+var doc = {};
+
+doc.list =
+`
+jquery
+jquery mobile
+jquery ui
+bootstrap 3
+bootstrap 4
+d3.js
+dojo
+ext core
+hammer.js
+indefinite observable
+moo tools
+myanmar tools
+prototype
+knockout.js
+angular.js
+angular.io
+react
+`;
+
+doc.d3 =
+`
+D3.js
+https://d3js.org/
+https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"></script>
+`;
+
+doc.dojo =
+`
+Dojo
+http://dojotoolkit.org/
+https://ajax.googleapis.com/ajax/libs/dojo/1.13.0/dojo/dojo.js
+<script src="https://ajax.googleapis.com/ajax/libs/dojo/1.13.0/dojo/dojo.js"></script>
+`;
+
+doc.extcore =
+`
+Ext Core
+http://www.sencha.com/products/extcore
+https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js
+<script src="https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js"></script>
+`;
+
+doc.hammerjs =
+`
+Hammer.JS
+http://hammerjs.github.io/
+https://ajax.googleapis.com/ajax/libs/hammerjs/2.0.8/hammer.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/hammerjs/2.0.8/hammer.min.js"></script>
+`;
+
+doc.indefinite =
+`
+Indefinite Observable
+https://github.com/material-motion/indefinite-observable-js/
+https://ajax.googleapis.com/ajax/libs/indefinite-observable/1.0.1/indefinite-observable.js
+<script src="https://ajax.googleapis.com/ajax/libs/indefinite-observable/1.0.1/indefinite-observable.js"></script>
+`;
+
+doc.jquery =
+`
+jQuery
+http://jquery.com/
+https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+`;
+
+doc.mootools =
+`
+MooTools
+http://mootools.net/
+https://ajax.googleapis.com/ajax/libs/mootools/1.6.0/mootools.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/mootools/1.6.0/mootools.min.js"></script>
+`;
+
+doc.prototype =
+`
+Prototype
+http://prototypejs.org/
+https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js
+<script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
+`;
+
+doc.scriptaculous =
+`
+Scrit.aculo.us
+http://script.aculo.us/
+https://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js
+<script src="https://ajax.googleapis.com/ajax/libs/scriptaculous/1.9.0/scriptaculous.js"></script>
+`;
+
+doc.shaka =
+`
+Shaka Player
+https://github.com/google/shaka-player/
+https://ajax.googleapis.com/ajax/libs/shaka-player/2.4.5/shaka-player.compiled.js
+<script src="https://ajax.googleapis.com/ajax/libs/shaka-player/2.4.5/shaka-player.compiled.js"></script>
+`;
+
+doc.spf =
+`
+SPF : Strcutured Page Fragments
+http://youtube.github.io/spfjs/
+https://ajax.googleapis.com/ajax/libs/spf/2.4.0/spf.js
+<script src="https://ajax.googleapis.com/ajax/libs/spf/2.4.0/spf.js"></script>
+`;
+
+doc.swfobject =
+`
+SWFObject
+https://github.com/swfobject/swfobject
+https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js
+<script src="https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+`;
+
+doc.threejs =
+`
+three.js
+http://threejs.org/
+https://ajax.googleapis.com/ajax/libs/threejs/r84/three.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/threejs/r84/three.min.js"></script>
+`;
+
+doc.webfontloader =
+`
+Web Font Loader
+https://github.com/typekit/webfontloader
+https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+`;
