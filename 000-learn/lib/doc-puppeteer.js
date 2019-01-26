9ef597f696632b41617eea13a3670afb7c144c0c
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-26 16:26:00 +0800

    puppeteer browser

--- 000-learn/lib/doc-puppeteer.js
+++ 000-learn/lib/doc-puppeteer.js
@@ -11,4 +11,16 @@ chromium r609904 137.9MB
 doc.snippets =
 `
 const puppeteer = require('puppeteer');
+page.setContent(html);
+`;
+
+doc.links =
+`
+https://developers.google.com/web/tools/puppeteer/
+https://developers.google.com/web/tools/puppeteer/get-started
+https://pptr.dev/#?product=Puppeteer&version=v1.11.0&show=outline
+https://developers.google.com/web/tools/puppeteer/examples
+https://developers.google.com/web/tools/puppeteer/articles/ssr
+https://github.com/GoogleChrome/puppeteer
+https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagesetcontenthtml
 `;
