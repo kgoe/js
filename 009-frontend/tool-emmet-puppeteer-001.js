36f2f385855974cabf66320d8d329cd10014c721
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-26 19:27:48 +0800

    puppeteer parset 001

--- 009-frontend/tool-emmet-puppeteer-001.js
+++ 009-frontend/tool-emmet-puppeteer-001.js
@@ -7,8 +7,36 @@ const sniph002 = 'html>(head>(title+meta+link+style+script>{var initprop=Object.
 var html = emmet.expandAbbreviation(sniph002);
 
 html2 = html.replace(/\$\{0\}/g, '');
+// console.log(html2);
 
-console.log(html2);
+/**
+ * compareProperties
+ * @description compares 2 json objects, returns properties not present in p1
+ * @param {string} p1
+ * @param {string} p2
+ * @return {*}
+ */
+function compareProperties( p1, p2 ) {
+  if ( typeof p1 == 'string' && typeof p2 == 'string' ) {
+    var pp1 = JSON.parse(p1);
+    var pp2 = JSON.parse(p2);
+    var pp1l = pp1.length;
+    var pp2l = pp2.length;
+    var set = [];
+    var unset = [];
+    for ( itemcnt = 0; itemcnt < pp2l; itemcnt ++ ) {
+      var theItem = pp2[itemcnt];
+      if ( pp1.indexOf(theItem) >= 0 ) {
+        set.push(theItem);
+      } else {
+        unset.push(theItem);
+      }
+    }
+    return unset;
+  } else {
+    return false;
+  }
+}
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -28,8 +56,9 @@ console.log(html2);
     };
   });
 
-  await console.log('Dimensions:', dimensions);
+  // await console.log('Dimensions:', dimensions);
   // await console.log(page);
+  await console.log(compareProperties(dimensions.init, dimensions.props));
 
   await browser.close();
 })();
