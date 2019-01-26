14806eb9ee7aadf8ad662c4e507af05d4823d2e8
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-26 23:04:24 +0800

    google + cdn updates

--- 004-js-library/doc/doc-google-cdn-modules.js
+++ 004-js-library/doc/doc-google-cdn-modules.js
@@ -23,12 +23,60 @@ angular.io
 react
 `;
 
+doc.jquery =
+`
+jQuery
+http://jquery.com/
+https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+`;
+
+doc.bootstrap =
+`
+https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css
+https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js
+https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js
+`;
+
+doc.bootstrap4 =
+`
+https://getbootstrap.com/docs/4.0/getting-started/introduction/
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+`;
+
 doc.d3 =
 `
 D3.js
 https://d3js.org/
 https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js
 <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"></script>
+`;
+
+doc.jqueryui =
+`
+jQuery UI
+http://jqueryui.com/
+https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+`;
+
+doc.jquerymobile =
+`
+jQuery Mobile
+http://jquerymobile.com/
+https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
 `;
 
 doc.dojo =
@@ -61,18 +109,6 @@ Indefinite Observable
 https://github.com/material-motion/indefinite-observable-js/
 https://ajax.googleapis.com/ajax/libs/indefinite-observable/1.0.1/indefinite-observable.js
 <script src="https://ajax.googleapis.com/ajax/libs/indefinite-observable/1.0.1/indefinite-observable.js"></script>
-`;
-
-doc.jquery =
-`
-jQuery
-http://jquery.com/
-https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 `;
 
 doc.mootools =
@@ -137,4 +173,14 @@ Web Font Loader
 https://github.com/typekit/webfontloader
 https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js
 <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+`;
+
+doc.myanmartools =
+`
+Myanmar Tools
+https://github.com/googlei18n/myanmar-tools/
+https://ajax.googleapis.com/ajax/libs/myanmar-tools/1.1.0/zawgyi_detector.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/myanmar-tools/1.1.0/zawgyi_detector.min.js"></script>
+https://ajax.googleapis.com/ajax/libs/myanmar-tools/1.1.0/zawgyi_converter.min.js
+<script src="https://ajax.googleapis.com/ajax/libs/myanmar-tools/1.1.0/zawgyi_converter.min.js"></script>
 `;
