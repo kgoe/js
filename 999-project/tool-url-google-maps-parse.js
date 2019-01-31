e525c0a2987b68168843b9add0dda6a2d6d9645c
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-31 16:49:27 +0800

    node url generation

---
+++ 999-project/tool-url-google-maps-parse.js
@@ -0,0 +1,17 @@
+;
+
+var url = require('url');
+
+var g001 = 'https://www.google.com.ph/maps/@14.6383512,121.0392434,15z';
+
+// console.log(url.parse(g001));
+// console.log(url.resolve(g001,''));
+// console.log(url.resolveObject(g001,''));
+
+/*
+window.location.protocol = "http:"
+window.location.host = "css-tricks.com"
+window.location.pathname = "example/index.html"
+window.location.search = "?s=flexbox"
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
+*/
