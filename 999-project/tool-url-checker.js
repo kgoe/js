4abfc477c85540c5662c4e81cbf111e20debd80f
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-31 16:41:02 +0800

    url checker

---
+++ 999-project/tool-url-checker.js
@@ -0,0 +1,54 @@
+const url = require('url');
+
+// console.log(url);
+/*
+url.Url()
+url.parse()
+url.resolve()
+url.resolveObject()
+url.URL()
+url.urlSearchParams()
+url.domainToASCII()
+url.domainToUnicode()
+url.pathToFileURL()
+url.fileURLToPath()
+*/
+
+// console.log(url.parse('https://www.google.com'));
+// console.log(url.resolve('https://www.google.com', '/'));
+/*
+Url {
+  protocol: 'https:',
+  slashes: true,
+  auth: null,
+  host: 'www.google.com',
+  port: null,
+  hostname: 'www.google.com',
+  hash: null,
+  search: null,
+  query: null,
+  pathname: '/',
+  path: '/',
+  href: 'https://www.google.com/' 
+}
+*/
+
+// console.log(url.pathToFileURL('file'));
+/*
+URL {
+  href: 'file:///C:/doc/kg-dev/gg-js/999-project/file',
+  origin: 'null',
+  protocol: 'file:',
+  username: '',
+  password: '',
+  host: '',
+  hostname: '',
+  port: '',
+  pathname: '/C:/doc/kg-dev/gg-js/999-project/file',
+  search: '',
+  searchParams: URLSearchParams {},
+  hash: '' 
+}
+*/
+
+
