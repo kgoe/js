a9cdd9f2975a214cadfe4e7524d4d61bf2fa7efb
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-01-31 16:11:55 +0800

    incrementor

--- data/read-write-data.js
+++ data/read-write-data.js
@@ -50,32 +50,97 @@ function dateyymmddhhmmss( sep ) {
   return output;
 }
 
+/**
+ * nsCreator
+ * @param {string} path
+ * @param {*} value
+ * @param {*} parent ?
+ */
+function nsCreator( path, value, parent ) {
+  var theParent = typeof parent !== 'undefined' ? parent : typeof global !== 'undefined' ? global : window;
+  var thePath = path.split('.');
+  var theReference = [];
+  var pathContext = false;
+
+  for ( var nsLvl = 0; nsLvl < thePath.length; nsLvl ++ ) {
+    if ( nsLvl > 3 ) {
+      console.log('info : level > 3');
+    }
+    pathContext = thePath[nsLvl];
+
+    if ( nsLvl == 0 ) {
+      if ( typeof theParent[pathContext] == 'undefined' ) {
+        if ( nsLvl == thePath.length - 1 ) {
+          theParent[pathContext] = value;
+        } else {
+          theParent[pathContext] = {};
+        }
+        theReference[nsLvl] = theParent[pathContext];
+      } else {
+        theReference[nsLvl] = theParent[pathContext];
+      }
+    } else {
+      if ( typeof theReference[nsLvl - 1][pathContext] == 'undefined' ) {
+        if ( nsLvl == thePath.length - 1 ) {
+          theReference[nsLvl - 1][pathContext] = value;
+        } else {
+          theReference[nsLvl - 1][pathContext] = {};
+        }
+        theReference[nsLvl] = theReference[nsLvl - 1][pathContext];
+      } else {
+        theReference[nsLvl] = theReference[nsLvl - 1][pathContext];
+      }
+    }
+  }
+
+  return theReference[theReference.length - 1];
+}
+
+/**
+ * nsChecker
+ * @param {string} path
+ * @param {*} parent ?
+ */
+function nsChecker( path, parent ) {
+  var theParent = typeof parent !== 'undefined'
+    ? parent : typeof global !== 'undefined'
+    ? global : window;
+  var thePath = path.split('.');
+  var theReference = [];
+  var pathContext = false;
+
+  for ( var nsLvl = 0; nsLvl < thePath.length; nsLvl ++ ) {
+    if ( nsLvl > 3 ) {
+      console.log('info : level > 3');
+    }
+    pathContext = thePath[nsLvl];
+    if ( nsLvl == 0 ) {
+      theReference[nsLvl] = theParent[pathContext];
+    } else {
+      theReference[nsLvl] = theReference[nsLvl - 1][pathContext];
+    }
+    // console.log(pathContext);
+    if ( typeof theReference[nsLvl] == 'undefined' ) {
+      console.log(pathContext);
+      return false;
+    } else if ( nsLvl == thePath.length - 1 ) {
+      return theReference[nsLvl];
+    }
+  }
+}
+
 var filename = dateyymmdd() + '-data.json';
 
 var filecontent = fs.readFileSync(filename);
 
 var data = JSON.parse(filecontent);
 
-if ( typeof data['2019'] !== 'undefined'
-  && typeof data['2019']['01'] !== 'undefined'
-  && typeof data['2019']['01']['count'] !== 'undefined' ) {
+var count = nsChecker('2019.01.count', data);
+
+if ( count !== false ) {
   data['2019']['01']['count'] ++;
 } else {
-  data['2019'] = {};
-  data['2019']['01'] = {};
-  data['2019']['01']['count'] = 0;
+  nsCreator('2019.01.count', 0, data);
 }
-
-/*
-var data = {};
-
-data.name = "test";
-
-data['2019'] = {};
-
-data['2019']['01'] = {
-  count: 0,
-};
-*/
 
 fs.writeFileSync(filename, JSON.stringify(data));
