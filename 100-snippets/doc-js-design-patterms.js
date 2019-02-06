f177d2dd4624d5519ba168b231e7ee61b2acaa40
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-02-06 12:01:03 +0800

    js design patterns topics

--- 100-snippets/doc-js-design-patterms.js
+++ 100-snippets/doc-js-design-patterms.js
@@ -6,7 +6,7 @@ var doc = {};
 
 doc.links =
 `
-https://addyosmani.com/resources/essentialjsdesignpatterns/book/#whatisapattern
+https://addyosmani.com/resources/essentialjsdesignpatterns/book
 `;
 
 doc.toc =
@@ -61,4 +61,54 @@ doc.what_is_a_pattern =
 patterns are proven solutions
 patterns can be easily reused
 patterns can be expressive
+
+reusing patterns assists 
+in preventing minor issues 
+that can cause major problems 
+in the applicattion development process.
+
+patterns can provide generalized solutions 
+which are documented in a fashion 
+that doesn't require them to be tied 
+to a specific problem.
+
+certain patterns can actually decrease
+the overall file-size footprint of our code
+by avoiding repetition.
+
+patterns add to a developer's vocabulary,
+which makes communication faster.
+
+patterns that are frequently used
+can be improved over time by harnessing
+the collective experiences other developers
+using those patterns contribute back to the
+design pattern community.
+
+solves a particular problem
+the solution to this problem cannot be obvious
+the concept described must have been proven
+it must describe a relationship
+
+fitness of purpose
+usefulness
+applicability
+
+a context
+a system of forces that a arises in that context
+a configuration that allows these forces to resolve themselves in context
+
+pattern name
+pattern description
+context outline
+problem statement
+solution
+design
+implementation
+illustrations
+examples
+co-requesites
+relations
+known usage
+discussions
 `;
