0ade207eaf7c6d11c4c575c7612db8d606a5c12f
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-02-07 12:57:43 +0800

    http and response validation

--- @010-js-parse/http-001.js
+++ @010-js-parse/http-001.js
@@ -30,6 +30,7 @@ var httpBasicPromise = function httpBasicPromise(theHost, thePath) {
    * httpBasicGetResponse
    * @param {*} theRequest
    * @param {*} theCallback
+   * @return {*} requestHandler
    */
   var httpBasicGetResponse = function httpBasicGetResponse(theRequest, theCallback) {
     var requestCallback = function requestCallback(response) {
@@ -43,6 +44,8 @@ var httpBasicPromise = function httpBasicPromise(theHost, thePath) {
       });
     };
     var requestHandler = http.get(theRequest, requestCallback);
+
+    return requestHandler;
   };
 
   var theRequest = formatRequest(theHost, thePath);
@@ -54,4 +57,31 @@ var httpBasicPromise = function httpBasicPromise(theHost, thePath) {
   return thePromise;
 };
 
-httpBasicPromise('www.google.com', '/?gws_rd=ssl').then(console.log);
+/**
+ * validateOutput
+ * @param {*} input
+ * @return {object} output
+ */
+function valiateHttpOutput( input ) {
+  var isHTML = /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/i.test(input);
+
+  try {
+    var isJSON = typeof JSON.parse(input) == 'object' ? true : false;
+  } catch ( err ) {
+    var isJSON = false;
+    console.log('json error', err);
+  }
+
+  var output = {
+    isHTML: isHTML,
+    isJSON: isJSON,
+    isStr: typeof input == 'string',
+    isNum: typeof input == 'number',
+    // body: input,
+  };
+
+  console.log(output);
+  return output;
+}
+
+httpBasicPromise('www.google.com', '/?gws_rd=ssl').then(valiateHttpOutput);
