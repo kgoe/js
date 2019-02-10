ffc24dca89c236e7f163df33320fb02732ea3a07
kgoespiritu <kgoespiritu@gmail.com>
2019-02-11 02:27:01 +0800

    javascript libraries

---
+++ _004-library/doc-list-machine-learning.js
@@ -0,0 +1,31 @@
+;
+
+var doc =
+`
+`;
+
+doc.links =
+`
+https://www.npmjs.com/package/machine_learning
+https://www.npmjs.com/package/brain.js
+https://www.npmjs.com/package/ml5
+https://www.npmjs.com/package/@tensorscript/core
+https://www.npmjs.com/package/fuzzy.ai
+https://www.npmjs.com/package/ml-sentiment
+https://www.npmjs.com/package/ml-matrix
+
+https://tutorialzine.com/2017/04/10-machine-learning-examples-in-javascript
+https://medium.com/tensorflow/introducing-tensorflow-js-machine-learning-in-javascript-bf3eab376db
+https://burakkanber.com/blog/machine-learning-in-other-languages-introduction/
+
+https://github.com/BrainJS/brain.js
+https://github.com/tensorflow/playground
+https://github.com/xviniette/FlappyLearning
+https://github.com/cazala/synaptic
+https://lines.chromeexperiments.com/
+https://github.com/karpathy/convnetjs
+https://github.com/dmotz/thing-translator
+https://github.com/janhuenermann/neurojs
+https://github.com/junku901/machine_learning
+https://github.com/deepforge-dev/deepforge
+`;
--- test.js
+++ test.js
@@ -14,7 +14,7 @@ var path = require('path');
 
 // Instantiate a Mocha instance.
 var mocha = new Mocha();
-var testDir = '@008-testing';
+var testDir = '_008-testing';
 
 // Add each .js file to the mocha instance
 fs.readdirSync(testDir)
