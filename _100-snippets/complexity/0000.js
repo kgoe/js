1a7a2b91d67d8ece8d54225ba74d3df7a8631ece
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-02-11 13:39:15 +0800

    updating complexity notes

--- _100-snippets/complexity/0000.js
+++
@@ -1,5 +0,0 @@
-var first = 1;
-var second = 2;
-
-first.toString();
-second.toString();
--- _100-snippets/complexity/0003-m.js
+++
@@ -1,8 +0,0 @@
-// mdule design pattern : scotch.io deval patel
-
-(function() {
-  // declare private variables and/or functions
-  return {
-    // declare public variables and/or functions
-  };
-})();
