620501102b6006951ea0249d5cff7b9808abff69
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-15 18:39:38 +0800

    js topics

---
+++ 000-js-topics/js-topics-000.js
@@ -0,0 +1,29 @@
+;doc={};
+
+/**
+ * template
+ */
+((doc)=>{
+})(doc);
+
+/**
+ * topics
+ */
+((doc)=>{
+
+doc.topics=
+`
+https://devdocs.io/javascript/
+https://devdocs.io/dom/
+https://devdocs.io/dom_events/
+https://devdocs.io/node~10_lts/
+
+javascript core
+javascript dom
+javascript dom events
+javascript browser
+javascript web api
+javascript node.js
+javascript web libraries
+`;
+})(doc);
