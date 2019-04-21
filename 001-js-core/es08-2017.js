4c2a8ae643b0b3f43d56064e77ed97ce56841675
kgoe-tablet <kgoespiritu@gmail.com>
2019-04-21 13:40:25 +0800

    updates

---
+++ 001-js-core/es08-2017.js
@@ -0,0 +1,31 @@
+;doc={};
+
+doc.topics=
+`
+01|scope
+02|conformance
+03|normative references
+04|overview
+05|notational conventions
+06|ecmascript data types and values
+07|abstract operations
+08|executable code and execution contexts
+09|ordinary and exotic object behaviours
+10|ecmascript language: source code
+11|ecmascript language: lexical grammar
+12|ecmascript language: expressions
+13|ecmascript language: statements and declarations
+14|ecmascript language: functions and classes
+15|ecmascript language: scripts and modules
+16|error handling and language extensions
+17|ecmascript standard built in objects
+18|the global object
+19|fundamental object
+20|numbers and dates
+0A|grammar summary
+0B|compatability
+0C|the strict mode of ecmascript
+0D|corrections and clarifications in the 5th edition with possible 3rd edition compatability impact
+0E|additions and changes in the 5th edition that introduce incomoatabilities with the 3rd edition
+0F|technically significant corrections and clarifications in the 5.1 edition
+`;
