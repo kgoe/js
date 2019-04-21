7f64fb90de042edf419f3a5cce32015d4ed9811a
kgoe-tablet <kgoespiritu@gmail.com>
2019-04-21 11:28:03 +0800

    es docs

---
+++ 001-js-core/es03.js
@@ -0,0 +1,24 @@
+;doc={};
+
+doc.topics=
+`
+01|scope
+02|conformance
+03|normative references
+04|overview
+05|notational conventions
+06|source text
+07|lexical conventions
+08|types
+09|type conversion
+10|execution contexts
+11|expressions
+12|statements
+13|function defenition
+14|program
+15|native ecmascript objects
+16|errors
+0A|grammar summary
+0B|compstability
+0
+`;
