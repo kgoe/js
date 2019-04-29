9f334e0a99c76b116d045aebc6a70601640e5961
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-29 18:47:06 +0800

    es standards updates

---
+++ 006-standards/ecma-standard/ecma-262-1st-edition-doc.js
@@ -0,0 +1,27 @@
+;doc={};
+
+
+/*
+ * template
+ */
+(doc=>{
+doc.template=
+`
+01|Scope
+02|Conformance
+03|References
+04|Overview
+05|Notational Conventions
+06|Source Text
+07|Lexical Conventions
+08|Types
+09|Type Conversion
+10|Execution Contexts
+11|Expressions
+12|Statements
+13|Function Definition
+14|Program
+15|Native ECMAScript objects
+16|Errors
+`;
+})(doc);
