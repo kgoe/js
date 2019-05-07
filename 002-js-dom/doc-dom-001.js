83ec8ef1b77f6028c07a154d32b7eabe3004967b
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-05-07 20:27:19 +0800

    updates

---
+++ 002-js-dom/doc-dom-001.js
@@ -0,0 +1,35 @@
+;doc={};
+
+
+doc.template=
+[
+`
+`
+];
+
+
+doc.topic=
+[
+`
+JavaScript DOM
+JavaScript Document Object Model
+`
+];
+
+
+doc.properties=
+[
+`
+Attr
+Node
+Element
+Document
+Window
+
+element |HTMLElement
+html    |HTMLHtmlElement
+head    |HTMLHeadElement
+body    |HTMLBodyElement
+`
+];
+
