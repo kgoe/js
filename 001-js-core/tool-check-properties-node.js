2dbd7e972141db98dddc8770afd5b7aee0927e44
kgoespiritu <kgoespiritu@gmail.com>
2019-05-11 13:54:09 +0800

    properties node

---
+++ 001-js-core/tool-check-properties-node.js
@@ -0,0 +1,72 @@
+;doc={};
+
+
+doc.template=
+[
+`
+`
+];
+
+
+var core = require('./properties-javascript-node.js');
+
+var helper = require('./tool-core-helper.js');
+
+var getGlobalRoot = helper.getGlobalRoot;
+var splitTemplateVariables = helper.splitTemplateVariables;
+
+if ( typeof global !== 'undefined' ) {
+  main();
+}
+
+/**
+ * main
+ */
+function main() {
+  var root = getGlobalRoot();
+  /**
+   * forEachLine
+   * @param {*} line
+   */
+  function forEachLine(line) {
+    line = line.split('|');
+
+    var theProperty = line[0];
+    var theType = typeof line[1] == 'string' ? line[1] : false;
+    var typeCheck = (theType) ?
+      typeof root[theProperty] == theType :
+        typeof root[theProperty] !== 'undefined';
+
+    if ( theProperty == 'null' ) {
+      typeCheck = typeof null == theType;
+    } else if ( theProperty == 'arguments' ) {
+      typeCheck = typeof arguments == theType;
+    } else if ( theProperty.indexOf('.') === -1 ) {
+      // check without namespace
+    } else {
+      // todo : check with namespace
+    }
+
+    // output
+    // typeCheck = false; // Force
+    if ( ! typeCheck ) {
+      console.log(theProperty, theType, typeCheck);
+    }
+  }
+
+  /**
+   * forEachCategory
+   * @param {*} cat
+   */
+  function forEachCategory(cat) {
+    try {
+      console.log('>>> Item :', cat);
+      var lines = splitTemplateVariables(core[cat][0]);
+      lines.forEach(forEachLine);
+    } catch(err) {
+      console.log('ERROR', cat, err);
+    }
+  }
+
+  Object.getOwnPropertyNames(core).forEach(forEachCategory);
+}
