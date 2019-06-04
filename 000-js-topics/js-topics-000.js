7de8a39c6526d35fcd0b1f86f56d77730ddfc0a6
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-06-04 18:58:13 +0800

    mdn web api

--- 000-js-topics/js-topics-000.js
+++ 000-js-topics/js-topics-000.js
@@ -1,4 +1,4 @@
-;doc={};
+;doc={};print=console.log;
 
 
 doc.template=
@@ -53,9 +53,97 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript#Tools_resources
 
 
 doc.table_of_contents=
-[
+{
+
+// MDN
+'js':
 `
+<<< tutorials >>>
+complete beginners
+intermediate
+advanced
+<<< references >>>
+built-in objects
+expressions & operators
+statements & declarations
+functions
+classes
+errors
+misc
+new in javascript
+<<< documentation >>>
+useful lists
+contribute
+`,
+
+// MDN
+'js-api':
 `
-,
-];
+Ambient Light Events
+Background Tasks
+Battery API 
+Beacon
+Bluetooth API
+Broadcast Channel API
+CSS Counter Styles
+CSS Font Loading API 
+CSSOM
+Canvas API
+Channel Messaging API
+Credential Management API
+DOM
+Encoding API
+Encrypted Media Extensions
+Fetch API 
+File System API 
+Frame Timing API
+Fullscreen API
+Gamepad API
+HTML Drag and Drop API
+High Resolution Time
+Image Capture API
+IndexedDB
+Long Tasks API 
+Media Capabilities API 
+Media Capture and Streams
+Media Session API
+Media Source Extensions 
+MediaStream Recording
+Navigation Timing
+Network Information API 
+Page Visibility API
+Payment Request API
+Performance API
+Performance Timeline API
+Permissions API
+Pointer Events
+Pointer Lock API
+Proximity Events 
+Push API 
+Resource Timing API
+Server Sent Events
+Service Workers API
+Storage
+Streams 
+Touch Events
+Vibration API
+Web Animations 
+Web Audio API
+Web Authentication API
+Web Crypto API
+Web Notifications
+Web Storage API
+Web Workers API
+WebGL
+WebRTC
+WebVR API 
+WebVTT
+Websockets API
+`,
+
+// MDN
+'js-interfaces':
+`
+`,
+}
 
