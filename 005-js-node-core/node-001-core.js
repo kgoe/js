c36cf92ec95b3c10fe5907b6ba190269096ffef4
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-06-04 19:54:23 +0800

    node core

---
+++ 005-js-node-core/node-001-core.js
@@ -0,0 +1,28 @@
+;doc={};print=console.log;
+
+/**
+ * template
+ */
+((doc)=>{
+})(doc); ;doc={};
+
+/**
+ * template
+ */
+((doc)=>{
+})(doc);
+
+/**
+ * js node core properties
+ */
+((doc)=>{
+
+doc.node_core_properties_links=
+`
+`;
+
+doc.node_core_properties=
+`
+`;
+
+})(doc);
