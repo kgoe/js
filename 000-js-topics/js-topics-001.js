aca58b7c6b2aae07c798e466f2e9e78a6ef09b56
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-06-15 19:22:57 +0800

    topics updates

---
+++ 000-js-topics/js-topics-001.js
@@ -0,0 +1,37 @@
+;doc={};
+;app={};
+;const print={};
+
+
+doc.template=
+[
+`
+`
+];
+
+
+doc.topics=
+[
+`
+# mdn javascript core topics
+
+## tutorials
+complete beginners
+intermediate
+advanced
+
+## references
+built-in objects
+expressions & operators
+statements & declarations
+functions
+classes
+errors
+misc
+new in javascript
+
+## documentation
+useful lists
+contribute
+`
+];
