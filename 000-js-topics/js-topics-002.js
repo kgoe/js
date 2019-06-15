f3fc94f9accb012941b0397644c675e47061567d
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-06-15 19:23:14 +0800

    topics updates api

---
+++ 000-js-topics/js-topics-002.js
@@ -0,0 +1,79 @@
+;doc={};
+;app={};
+;const print={};
+
+
+doc.template=
+[
+`
+`
+];
+
+
+doc.topics=
+[
+`
+# mdn javascript api
+
+Ambient Light Events
+Background Tasks
+Battery API 
+Beacon
+Bluetooth API
+Broadcast Channel API
+CSS Counter Styles
+CSS Font Loading API 
+CSSOM
+Canvas API
+Channel Messaging API
+Credential Management API
+DOM
+Encoding API
+Encrypted Media Extensions
+Fetch API 
+File System API 
+Frame Timing API
+Fullscreen API
+Gamepad API
+HTML Drag and Drop API
+High Resolution Time
+Image Capture API
+IndexedDB
+Long Tasks API 
+Media Capabilities API 
+Media Capture and Streams
+Media Session API
+Media Source Extensions 
+MediaStream Recording
+Navigation Timing
+Network Information API 
+Page Visibility API
+Payment Request API
+Performance API
+Performance Timeline API
+Permissions API
+Pointer Events
+Pointer Lock API
+Proximity Events 
+Push API 
+Resource Timing API
+Server Sent Events
+Service Workers API
+Storage
+Streams 
+Touch Events
+Vibration API
+Web Animations 
+Web Audio API
+Web Authentication API
+Web Crypto API
+Web Notifications
+Web Storage API
+Web Workers API
+WebGL
+WebRTC
+WebVR API 
+WebVTT
+Websockets API
+`
+];
