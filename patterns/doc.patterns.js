a36e03cd9fc3971daea29c84cfb72d465427e9c3
kgoespiritu <kgoespiritu@gmail.com>
2019-11-16 13:42:22 +0800

    patterns ideas

---
+++ patterns/doc.patterns.js
@@ -0,0 +1,20 @@
+;doc={};
+
+
+doc.toc=
+`
+patterns
+js
+js design patterns
+js design patterns category
+js design patterns hierarchy
+`;
+
+
+doc.hierarchy=
+`
+dp0.snippet
+dp1.template
+dp2.standard
+dp3.pattern
+`;
